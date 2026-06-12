8794df1622e91935187a35b020948b8370b7a7be
Henrique Alves Vasconselos <henrique.vasconselos@novacap.df.gov.br>
2026-06-12 14:46:06 -0300

    Troca banner do evento

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section id="hero" className="w-full scroll-mt-24 md:scroll-mt-28">
       <img
-        src="/assets/banner.jfif"
+        src="/assets/banner.png"
         alt="Feirão do Trabalhador"
         className="w-full h-auto block"
       />
